3190323c8d823be9bfc08eb0d35b3ae8e2944958
Hizkia Luke <contact.hizkia@gmail.com>
2022-09-08 18:45:29 +0700

    update db config

--- config/database.js
+++ config/database.js
@@ -1,9 +1,9 @@
 const mysql = require('mysql');
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'test',
+    host: 'us-cdbr-east-06.cleardb.net',
+    user: 'b0d9c584d28992',
+    password: '3e0eefa9',
+    database: 'heroku_d00307e00dbc214',
     multipleStatements: true
 });
 
