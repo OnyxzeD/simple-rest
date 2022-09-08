11d9b33c5a5273ed9c1878395f36df007c807887
Hizkia Luke <contact.hizkia@gmail.com>
2022-09-08 20:54:40 +0700

    fix db config

--- config/database.js
+++ config/database.js
@@ -6,10 +6,4 @@ var connection = mysql.createPool({
     database: 'heroku_d00307e00dbc214',
     multipleStatements: true
 });
-
-// connect to database
-connection.connect((err) => {
-    if (err) throw err;
-    console.log('MySQL Connected...');
-});
 module.exports = connection;
